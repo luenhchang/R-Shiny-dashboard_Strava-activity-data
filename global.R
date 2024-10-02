665288a5f364552360b1383f9d5c4aec42fa673b
Lun-Hsien Chang <luenhchang@gmail.com>
2024-10-02 21:05:55 +1000

    Converted UTC datetime to AEST datetime using format(start.datetime.UTC, tz="Australia/Brisbane",usetz=TRUE)

--- global.R
+++ global.R
@@ -285,32 +285,63 @@ ride <- act_data |>
 #   act_id=11826580247
 #   ,stoken
 #   ,units = "metric") # dim(speed.splits.11826580247) 41 3
-format.datetime <- "%b %d, %Y, %I:%M:%S %p"
+
+# Load the lubridate package
+library(lubridate)
+
+# Define the UTC datetime as a string
+utc_time_string <- "2024-10-01 02:35:06"
+
+# Convert the string to POSIXct in UTC
+utc_time <- as.POSIXct(utc_time_string, format = "%Y-%m-%d %H:%M:%S", tz = "GMT")
+format(utc_time, tz="Australia/Brisbane",usetz=TRUE)
+
+# Convert to AEST (Australia/Brisbane)
+aest_time <- with_tz(utc_time, tzone = "Australia/Brisbane")
+
+df <- data.frame(..., stringsAsFactors=FALSE)
+
+df$utc_time_stamp <- as.POSIXct(df$utc_time_stamp, format="%Y-%m-%d %H:%M:%S", tz='GMT')
+
+tz_v <- Vectorize(function(x,y) {format(x, tz=y, usetz=TRUE)})
+
+df$new_local_time <- tz_v(df$utc_time_stamp, df$time_zone)
+df
+
+# Print the result
+print(aest_time)
 
 # 2024-10-01T02:35:06Z is in UTC. The "Z" at the end stands for Zulu time, which is another way to indicate UTC (Coordinated Universal Time).
 act_data.1 <- act_data %>%
   dplyr::select(id, start_date, name, gear_id, sport_type, distance, total_elevation_gain, elapsed_time, moving_time,average_heartrate, max_heartrate) %>%
-  dplyr::rename(start.datetime.UTC=start_date) %>%
   dplyr::mutate(
-    start.date.UTC=lubridate::date(start.datetime.UTC)
+     start.datetime.UTC=as.POSIXct(x=start_date, format ="%Y-%m-%dT%H:%M:%SZ", tz="GMT")
+    ,start.date.UTC=lubridate::date(start.datetime.UTC)
     ,start.datetime.local=case_when(
       start.date.UTC >= ymd('2023-01-16') & start.date.UTC <= ymd('2023-02-05')~
-        as.POSIXct(start.datetime.UTC, tz="Asia/Kuala_Lumpur")
-      ,start.date.UTC >= ymd('2023-09-02') & start.date.UTC <= ymd('2023-10-02') ~ as.POSIXct(start.datetime.UTC, tz="Asia/Taipei")
-      ,TRUE ~ as.POSIXct(start.datetime.UTC, tz="Australia/Brisbane")
+        format(start.datetime.UTC, tz="Asia/Kuala_Lumpur",usetz=TRUE)
+        #as.POSIXct(start.datetime.UTC, tz="Asia/Kuala_Lumpur")
+      ,start.date.UTC >= ymd('2023-09-02') & start.date.UTC <= ymd('2023-10-02') ~ 
+        #as.POSIXct(start.datetime.UTC, tz="Asia/Taipei")
+        format(start.datetime.UTC, tz="Asia/Taipei",usetz=TRUE)
+      #,TRUE ~ as.POSIXct(start.datetime.UTC, tz="Australia/Brisbane")
+      ,TRUE ~ format(start.datetime.UTC, tz="Australia/Brisbane",usetz=TRUE)
       )
     ,start.date.local=lubridate::date(start.datetime.local)
     ,start.year.local=lubridate::year(start.datetime.local)
     ,start.dayofyear.local=lubridate::yday(start.datetime.local)
     ,start.month.local=lubridate::month(start.datetime.local)
-    ,start.day.local=weekdays(start.datetime.local)
-    ,start.week.local=lubridate::week(start.datetime.local)
-    # Change distance to meters for swim later
+    ,start.day.local=weekdays(as.Date(start.datetime.local))
+    ,start.week.local=lubridate::week(as.Date(start.datetime.local))
     ,distance.km=distance
     ,elevation.gain.m= total_elevation_gain # Elevation gain in meters
     ,elapsed.time.hour=elapsed_time/60/60
     ,moving.time.hour= moving_time/60/60) %>%
-  dplyr::select(-distance, -total_elevation_gain, -elapsed_time, -moving_time) # dim(act_data.1) 922 19
+  dplyr::select(-distance, -total_elevation_gain, -elapsed_time, -moving_time) # dim(act_data.1) 922 20
+
+# Check the final result
+print(tail(act_data.1$start.datetime.local, n = 1))
+
 
 #------------------
 # Process 2023 data
@@ -325,55 +356,11 @@ activities.2023 <- act_data.1 %>%
     ,grepl(pattern="bike fitting", x=name, ignore.case=TRUE) ~ stringi::stri_trans_totitle("bike fitting")
     ,TRUE ~ sport_type )) # dim(activities.2023) 372 20
 
-#-----------------------
-# Analyse activities.csv
-#-----------------------
-# Download activities.csv from Google drive
-## https://drive.google.com/file/d/1bx8iaRnjf4jATc4yWnB0-McLlDFpJZRP/view?usp=drive_link
-df <- read.csv(file = file.path(dir.Strava.export_37641772,"activities.csv"), header = TRUE) # dim(df) 922 94
-
-format.datetime <- "%b %d, %Y, %I:%M:%S %p"
-
-activities.df <- df |> 
-  dplyr::select(Activity.ID, Activity.Date, Activity.Name, Activity.Gear, Activity.Type, Distance, Elevation.Gain, Elapsed.Time, Moving.Time, Max.Heart.Rate) |>
-  dplyr::mutate(
-    start.datetime.UTC=as.POSIXct(x=Activity.Date, format = format.datetime, tz="UTC")
-    ,start.date.UTC=lubridate::date(start.datetime.UTC)
-    ,start.datetime.local=case_when(
-      start.date.UTC >= ymd('2023-01-16') & start.date.UTC <= ymd('2023-02-05')~
-        as.POSIXct(start.datetime.UTC, tz="Asia/Kuala_Lumpur")
-      ,start.date.UTC >= ymd('2023-09-02') & start.date.UTC <= ymd('2023-10-02') ~ as.POSIXct(start.datetime.UTC, tz="Asia/Taipei")
-      ,TRUE ~ as.POSIXct(start.datetime.UTC, tz="Australia/Brisbane")
-    )
-    ,start.date.local=lubridate::date(start.datetime.local)
-    ,start.year.local=lubridate::year(start.datetime.local)
-    ,start.dayofyear.local=lubridate::yday(start.datetime.local)
-    ,start.month.local=lubridate::month(start.datetime.local)
-    ,start.day.local=weekdays(start.datetime.local)
-    ,start.week.local=lubridate::week(start.datetime.local)
-    # Distance contains decimal places for cycling running distance, and comma for swim distance
-    ,distance.km= dplyr::case_when(
-      Activity.Type=="Swim" ~ as.numeric(gsub(pattern=",", replacement=".", x=Distance))
-      ,Activity.Type %in% c("Ride","Run","E-Bike Ride", "Walk") ~ as.numeric(Distance)
-      ,TRUE ~ NA_integer_)
-    ,elevation.gain.m= Elevation.Gain # Elevation gain in meters
-    ,elapsed.time.hour=Elapsed.Time/60/60
-    ,moving.time.hour=Moving.Time/60/60) |>
-  dplyr::select(-Distance, -Elevation.Gain, -Elapsed.Time, -Moving.Time) # dim(activities.df) 922 19
-
-activities.2023 <- activities.df |> 
-    dplyr::filter(start.year.local==2023 & Activity.Type %in% c("Ride","Run","Swim","Workout","Walk")) |>
-    # Split Activity.Type="Workout"
-    dplyr::mutate(activity.type=dplyr::case_when(
-      grepl(pattern="table tennis", x=Activity.Name, ignore.case=TRUE) ~ 
-        stringi::stri_trans_totitle("table tennis")
-      ,grepl(pattern="badminton", x=Activity.Name, ignore.case=TRUE) ~ 
-        stringi::stri_trans_totitle("badminton")
-      ,grepl(pattern="rehabilitation exercise|strength and stability exercises|dry land exercises", x=Activity.Name, ignore.case=TRUE) ~ 
-        stringi::stri_trans_totitle("strength & stability workout")
-      ,grepl(pattern="bike fitting", x=Activity.Name, ignore.case=TRUE) ~ 
-        stringi::stri_trans_totitle("bike fitting")
-      ,TRUE ~ Activity.Type )) # dim(activities.2023) 341 20
+#------------------
+# Process 2024 data
+#------------------
+activities.2024 <- act_data.1 %>%
+  dplyr::filter(start.year.local==2024 & sport_type %in% c("Ride","Run","Swim","Workout","Walk"))
 
 #************************************************************************************************#
 #---------------------------------This is the end of this file ----------------------------------#
